76ec9a6c6af1dcf341c07fe76b3a77495a3b8ebe
Thomas Zerr <thomas.zerr@gmail.com>
2020-11-13 18:58:25 +0100

    -refactored: ApiService functions are now part of a class

--- ApiService.py
+++ ApiService.py
@@ -12,32 +12,64 @@ class RefereeType(Enum):
     LEFT = "L"
     RIGHT = "R"
 
-def login(username, password):
-    pass
+class ApiService:
+    token = None
+    competitionId = None
+    attemptLogId = None
 
-def getCurrentAthlete(competitionId):
-    response = requests.post("https://heavyplates.com/api/competition/{}/refereecurrentcompetitor".format(competitionId))
-    return handleResponse(response)
+    baseUrl = "https://heavyplates.com/api"
+    apiUrls = {
+        "login": "/login",
+        "currentAthlete": "/competition/{}/refereecurrentcompetitor",
+        "refereeDecision": "/competition/{}/refereedecision"
+    }
 
-def handleResponse(response):
-    if response.ok:
-        return response.json()
+    def __init__(self, username, password, competitionId):
+        self.login(username, password)
+        self.competitionId = competitionId
 
-    raise Exception("Error Code {} occured with payload '{}'".format(response.status_code, response.text))
+    def login(self, username, password):
+        response = requests.post(self._createUrl("login"), json = self._createLoginRequestBody(username, password))
+        self.token = self._getResponseBody(response)["token"]
 
-def sendRefereeDecision(competitionId, attemptLogId, refereeType, refereeDecision):
-    requestBody = createRefereeDecisionRequestBody(competitionId, attemptLogId, refereeType, refereeDecision)
-    response = requests.post("https://heavyplates.com/api/competition/{}/refereedecision".format(competitionId), requestBody)
-    return handleResponse(response)
+    def _createLoginRequestBody(self, username, password):
+        return {"username": username, "password": password}
 
-def createRefereeDecisionRequestBody(competitionId, attemptLogId, refereeType, refereeDecision):
-    requestBody = {"attemptLogId": attemptLogId, "competitionId": competitionId, "refereeType": refereeType.value, "refereeId": 0}
+    def _createUrl(self, name):
+        if name == "login":
+            return self.baseUrl + self.apiUrls[name]
+        elif name == "currentAthlete" or name == "refereeDecision":
+            return self.baseUrl + self.apiUrls[name].format(self.competitionId)
+        else:
+            raise Exception("Unknown URL Name '{}'".format(name))
 
-    if refereeDecision == RefereeDecision.WHITE:
-        requestBody["valid"] = True
-        requestBody["notValidReason"] = None
-    else:
-        requestBody["valid"] = False
-        requestBody["notValidReason"] = refereeDecision.value
+    def _getResponseBody(self, response):
+        if response.ok:
+            return response.json()
+        else:
+            raise Exception("Error Code {} occured with payload '{}'".format(response.status_code, response.text))
+    
+    def getCurrentAthlete(self):
+        response = requests.get(self._createUrl("currentAthlete"), headers = self._createAuthorizationHeader())
+        responseBody = self._getResponseBody(response)
+        self.attemptLogId = responseBody["attemptLogId"]
+        return responseBody
 
-    return requestBody
+    def _createAuthorizationHeader(self):
+        return {"Authorization": "Bearer {}".format(self.token)}
+
+    def sendRefereeDecision(self, refereeType, refereeDecision):
+        response = requests.post(self._createUrl("refereeDecision"), json = self._createRefereeDecisionRequestBody(refereeType, refereeDecision), headers = self._createAuthorizationHeader())
+        return self._getResponseBody(response)
+
+    def _createRefereeDecisionRequestBody(self, refereeType, refereeDecision):
+        requestBody = {"attemptLogId": self.attemptLogId, "competitionId": self.competitionId, "refereeType": refereeType.value, "refereeId": 0}
+
+        if refereeDecision == RefereeDecision.WHITE:
+            requestBody["valid"] = True
+            requestBody["notValidReason"] = None
+        else:
+            requestBody["valid"] = False
+            requestBody["notValidReason"] = refereeDecision.value
+
+        return requestBody
