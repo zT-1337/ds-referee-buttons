20891c6fb03b577f34e38ed152937e8cfd62de47
Thomas Zerr <thomas.zerr@gmail.com>
2020-12-14 14:39:27 +0100

    -added: polling interval as constructor parameter

--- CurrentAthleteRefresher.py
+++ CurrentAthleteRefresher.py
@@ -2,11 +2,13 @@ from threading import Timer
 
 class CurrentAthleteRefresher:
     apiService = None
-    pollingInterval = 5
+    pollingInterval = None
 
-    def __init__(self, apiService):
-        self._startCurrentAthletePolling()
+    def __init__(self, apiService, pollingInterval):
         self.apiService = apiService
+        self.pollingInterval = pollingInterval
+        self._startCurrentAthletePolling()
+        
 
     def _startCurrentAthletePolling(self):
         Timer(self.pollingInterval, self._pollCurrentAthlete).start()
